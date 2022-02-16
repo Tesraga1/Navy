3fc01fd11c04a11d0d64ed2cf354291b7e56a072
Julian A Blanco <jbproffesional6@gmail.com>
2022-02-16 16:47:34 -0600

    Made intake one button

--- src/intake.cpp
+++ src/intake.cpp
@@ -10,21 +10,25 @@ int biggiecheeie_port = 16;
 pros::Motor taketake(biggiecheeie_port, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 void set_take(int input) {taketake = input;}
 
+bool takeboy = false;
 void takeintake() {
-    bool takeboy = false;
-    int i = 0;
-    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP))
+
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_X))
     {
         ///set_mogo(127);
         if (takeboy == true) {
             takeboy = false;
+            pros::delay(100);
         } else {
             takeboy = true;
+            pros::delay(100);
         }
     }
-    if (takeboy = true) {
+    if (takeboy == true) {
         set_take(127);
+//
     } else {
         set_take(0);
+//
     }
 }
