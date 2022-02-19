2f0bd6d70c994f372338ef42ab63d158aea63f74
Julian A Blanco <jbproffesional6@gmail.com>
2022-02-19 12:13:53 -0600

    Changed intake value

--- src/intake.cpp
+++ src/intake.cpp
@@ -25,9 +25,9 @@ void takeintake() {
         }
     }
     if (takeboy == true) {
-        set_take(-127);
+        set_take(-110);
     } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
-        set_take(127);
+        set_take(110);
     } else {
         set_take(0);
     }
